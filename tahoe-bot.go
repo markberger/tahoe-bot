b7dfb05d3d5a7573b52a2c8beb6eda8185fc881a
Mark Berger <mark.berger.j@gmail.com>
2013-07-23 18:39:52 -0400

    Now identifies with NickServ

--- tahoe-bot.go
+++ tahoe-bot.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
 	"github.com/markberger/tally"
 )
 
+type Settings struct {
+	Password string
+}
+
 func main() {
+	var s Settings
+	f, err := ioutil.ReadFile("./password.json")
+	if err != nil {
+		fmt.Printf("Error loading password.json: %v\n", err)
+		return
+	}
+	err = json.Unmarshal(f, &s)
+	if err != nil {
+		fmt.Printf("Error unmarshalling password.json: %v\n", err)
+		return
+	}
+
 	tally.InitLogging()
 	bot := tally.NewBot()
 	bot.Connect()
+	bot.PrivateMsg("NickServ", "identify " + s.Password)
 	bot.Run()
 }
